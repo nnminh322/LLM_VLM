5ca9cbef09ef92294b396b524a367fe858b5fcb1
Nguyen Nhat Minh <nnminh322@gmail.com>
2026-01-16 09:02:03 +0700

    pylance alert fix dtype

--- LLM/src/core/base_processor.py
+++ LLM/src/core/base_processor.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 from core.base_inputs import BaseInput
-from typing import List, Dict, TypeVar, Generic, Optional
+from typing import List, Dict, TypeVar, Generic, Optional, Any
 from transformers import PreTrainedTokenizerBase, ImageProcessingMixin
 import torch
 
@@ -15,18 +15,27 @@ class BaseProcessor(Generic[I], ABC):
         self.image_processor: Optional[ImageProcessingMixin] = None
 
     @abstractmethod
-    def setup(self):
+    def setup(self) -> None:
         # set tokenizer and image processor (maybe from huggingface)
         pass
 
     @abstractmethod
-    def processes(self, inputs: I) -> Dict[str, torch.Tensor]:
+    # def processes(self, inputs: I) -> Dict[str, torch.Tensor]:
+    def processes(self, inputs: I) -> Dict[str, Any]:  # fuk you pylance
         pass
 
     @abstractmethod
-    def decode(self, token_ids):
+    def decode(self, token_ids: Any) -> str:
         pass
 
-    def apply_chat_tempplate(self, messages: List[Dict[str, str]]):
-        # will complete later
-        pass
+    def apply_chat_template(
+        self, messages: List[Dict[str, str]], is_inference: bool = True
+    ) -> str:
+        if self.tokenizer is None:
+            raise RuntimeError("Tokenizer chưa được setup! Hãy gọi .setup() trước.")
+
+        # Ép kiểu kết quả trả về chắc chắn là str
+        result = self.tokenizer.apply_chat_template(
+            messages, tokenize=False, add_generation_prompt=is_inference
+        )
+        return str(result)
--- LLM/src/processors/llm_processor.py
+++ LLM/src/processors/llm_processor.py
@@ -4,7 +4,7 @@ from core.base_inputs import TextInput
 from transformers import AutoTokenizer
 
 
-class LLMProcessor(BaseProcessor):
+class LLMProcessor(BaseProcessor[TextInput]):
     def __init__(self, model_name: str, config: Dict):
         super().__init__(model_name, config)
 
@@ -15,13 +15,19 @@ class LLMProcessor(BaseProcessor):
         )
 
     def processes(self, inputs: TextInput):
-        return self.tokenizer(
-            inputs.messages,
+        if self.tokenizer is None:
+            raise RuntimeError("Tokenizer is None. Did you call setup()?")
+        messages = self.apply_chat_template(inputs.messages)
+        encoding = self.tokenizer(
+            messages,
             return_tensors="pt",
             padding=True,
             truncation=True,
             max_length=inputs.max_length,
         )
+        return dict(encoding)
 
     def decode(self, token_ids):
-        return self.tokenizer.batch_decode(token_ids, skip_special_tokens=True)
+        if self.tokenizer is None:
+            raise RuntimeError("Tokenizer is None.")
+        return self.tokenizer.decode(token_ids=token_ids, skip_special_tokens=True)
